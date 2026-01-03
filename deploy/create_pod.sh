87948b45ab5e94a6b1f42830c0dc15ce6f26549f
Tim Marple <tim@maidenlabs.org>
2026-01-02 17:57:58 -0800

    Smaller chips

--- deploy/create_pod.sh
+++ deploy/create_pod.sh
@@ -3,7 +3,7 @@ set -e
 
 # --- CONFIGURATION ---
 POD_NAME="mental-health-ai-training"
-GPU_TYPE="NVIDIA A40" 
+GPU_TYPE="NVIDIA RTX 6000 Ada Generation" 
 GPU_COUNT=1
 IMAGE_NAME="timarple/mental-health-ai:latest"
 CONTAINER_DISK_SIZE=50
